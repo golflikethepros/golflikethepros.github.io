490716d60a83e63bdff2ad0f2a3e2e3a3b52bfbf
joeltjames <joeltjames@gmail.com>
2015-04-06 16:06:49 -0400

    MOreDAL

--- Scripts/cookieTest.js
+++ Scripts/cookieTest.js
@@ -1,9 +1,9 @@
 ﻿function doNotShow() {
-    document.cookie("doNotShow", "yes");
+
 }
 
 $(window).load(function () {
-    var doNotShow = document.cookie("doNotShow");
+    var doNotShow = $.cookie("doNotShow");
     if (doNotShow !== "yes") {
         $("#myModal").modal({
             show: true
